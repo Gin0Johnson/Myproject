55c6b90e9a5668a2e8bbf36aca338e2faa778969
Jino <Jino@LAPTOP-CTPBQ3G7>
2024-01-29 15:35:07 +0530

    hii

---
+++ src/Demo.java
@@ -0,0 +1,13 @@
+
+
+public class Demo {
+	public static void main(String[] args) {
+		try {
+			int arr1[]=new int[5];
+			arr1[6]=90;
+		
+		}catch(Exception e) {
+			System.out.println(e);
+		}
+		}
+}
